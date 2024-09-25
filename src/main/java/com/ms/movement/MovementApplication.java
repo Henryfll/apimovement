51a3e840bbf5325c939d4f9b1ea18dd349b937e6
Henry <henry.2154@hotmail.com>
2024-09-24 23:10:51 -0500

    initial commit

---
+++ src/main/java/com/ms/movement/MovementApplication.java
@@ -0,0 +1,13 @@
+package com.ms.movement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/movement/MovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.movement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
